36da87a9f9c93d26e8d9174e09cdde824ab5cd50
ka <todo1@rotarydef.net>
2023-09-22 23:00:20 -0700

    first

---
+++ CMakeKautilHeader.cmake
@@ -0,0 +1,13 @@
+if(NOT DEFINED KAUTIL_THIRD_PARTY_DIR)
+    set(KAUTIL_THIRD_PARTY_DIR ${CMAKE_BINARY_DIR})
+    file(MAKE_DIRECTORY "${KAUTIL_THIRD_PARTY_DIR}")
+endif()
+
+macro(git_clone url)
+    get_filename_component(file_name ${url} NAME)
+    if(NOT EXISTS ${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name})
+        file(DOWNLOAD ${url} "${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
+    endif()
+    include("${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
+    unset(file_name)
+endmacro()
